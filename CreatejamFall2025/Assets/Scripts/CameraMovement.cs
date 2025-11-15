316b26904dd0c0c82345245cad9146e96d6cb23f
TheCookieLeg <lucas2.olafsen@gmail.com>
2025-11-15 12:09:55 +0100

    Working on camera
    Still doesnt work, but progress is being made

---
+++ CreatejamFall2025/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using Unity.Cinemachine;
+using UnityEngine.Splines;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private CinemachineSplineDolly dollyCart;
+    [SerializeField] private SplineContainer smoothPath;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
