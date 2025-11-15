fd3d848329081eb2cf649bea8faa5b76a50222cc
paragoncode <54541723+paragoncode@users.noreply.github.com>
2025-11-15 23:05:57 +0100

    sounds and soundfxmanager script

---
+++ CreatejamFall2025/Assets/Scripts/SoundFXManager.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class SoundFXManager : MonoBehaviour
+{
+    public static SoundFXManager instance;
+
+    [SerializeField] private AudioSource soundFXObject;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public void PlaySoundFX(AudioClip audioClip, Transform spawnTransform, float volume)
+    {
+        //spawn in gameObject
+        AudioSource audioSource = Instantiate(soundFXObject, spawnTransform.position, Quaternion.identity);
+
+        //assign audioclip
+        audioSource.clip = audioClip;
+
+        //assign volume
+        audioSource.volume = volume;
+
+        //play sound
+        audioSource.Play();
+
+        //get length of sound FX clip
+        float clipLength = audioSource.clip.length;
+
+        //destroy audio gameObject
+        Destroy(audioSource.gameObject, clipLength);
+    }
+}
